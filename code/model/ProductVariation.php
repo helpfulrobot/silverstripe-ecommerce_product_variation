68dff3011e04ec2ae5d28421e4cfbc657c254077
Nicolaas <github@sunnysideup.co.nz>
2013-02-19 21:07:44 +0000

    IMPORTANT UPDATE
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_variation/tags/2.4@3974 950b781c-c544-efab-2f99-90083ee69683

--- code/model/ProductVariation.php
+++ code/model/ProductVariation.php
@@ -343,6 +343,11 @@ class ProductVariation extends DataObject implements BuyableModel{
 	}
 
 	protected $currentStageOfRequest = "";
+
+	protected $saveParentProduct = false;
+
+	function setSaveParentProduct($b) {$this->saveParentProduct = $b;}
+
 	/**
 	 * standard SS method
 	 * sets the FullName + FullSiteTreeSort of the variation
@@ -384,13 +389,21 @@ class ProductVariation extends DataObject implements BuyableModel{
 	 */
 	function onAfterWrite() {
 		parent::onAfterWrite();
+		//clean up data???
+		//todo: what is this for?
 		if(isset($_POST['ProductAttributes']) && is_array($_POST['ProductAttributes'])){
-			$this->AttributeValues()->setByIDList(array_values($_POST['ProductAttributes']));
+			$productAttributesArray = array();
+			foreach($_POST['ProductAttributes'] as $key => $value) {
+				$productAttributesArray[$key] = intval($value);
+			}
+			$this->AttributeValues()->setByIDList(array_values($productAttributesArray));
 		}
 		unset($_POST['ProductAttributes']);
-		if($product = $this->Product()) {
-			$product->writeToStage('Stage');
-			$product->publish('Stage', 'Live');
+		if($this->saveParentProduct) {
+			if($product = $this->Product()) {
+				$product->writeToStage('Stage');
+				$product->publish('Stage', 'Live');
+			}
 		}
 		Versioned::set_reading_mode($this->currentStageOfRequest);
 	}
